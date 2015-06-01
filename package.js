688ea3b8b42c226acb5d7237bdc65f5265fab859
Taras Dolynyuk <trsdln@gmail.com>
2015-06-01 14:37:57 +0300

    specified version of modernizr package using @

--- package.js
+++ package.js
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.2');
-  api.use('mrt:modernizr-meteor#2.6.2');
+  api.use('mrt:modernizr-meteor@2.6.2');
   api.addFiles(['position-sticky.js'], 'client');
 });
